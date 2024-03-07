bc0bdc77690ecf74937653fa6419378a58ead7f6
Lance02345 <lancelevis19@gmail.com>
2024-03-07 13:36:11 +0300

    updatedHome.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,49 +6,29 @@
     <!-- slider_area_start -->
     <div class="slider_area">
         <div class="slider_active owl-carousel">
-            <div class="single_slider d-flex align-items-center justify-content-center slider_bg_1">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xl-12">
-                            <div class="slider_text text-center">
-                                <h3 style="font-size:56px;">Skifun Training Center</h3>
-                                <p>Unlock to enjoy the view of Martine</p>
-                            </div>
-                        </div>
-                    </div>
+        <div class="single_slider d-flex align-items-center justify-content-center slider_bg_1">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="slider_text text-center">
+                <h3 style="font-size: 56px; font-weight: bold; font-family: Arial, sans-serif;">
+    <span style="color: #9FE2BF;">Ski</span>fun Training Center
+</h3>                    <p style = "font-size:20px; font-weight: bold;">Experience the largest indoor <br> winter facility in Kenya.</p>
                 </div>
             </div>
+        </div>
+    </div>
+</div>
             <div class="single_slider  d-flex align-items-center justify-content-center slider_bg_2">
                 <div class="container">
                     <div class="row">
                         <div class="col-xl-12">
                             <div class="slider_text text-center">
-                                <h3  style="font-size:56px;">Skifun Training Center</h3>
-                                <p>Unlock to enjoy the view of Martine</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="single_slider d-flex align-items-center justify-content-center slider_bg_1">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xl-12">
                             <div class="slider_text text-center">
-                                <h3  style="font-size:56px;">Skifun Training Center</h3>
-                                <p>Unlock to enjoy the view of Martine</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="single_slider  d-flex align-items-center justify-content-center slider_bg_2">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xl-12">
-                            <div class="slider_text text-center">
-                                <h3  style="font-size:56px;">Skifun Training Center</h3>
-                                <p>Unlock to enjoy the view of Martine</p>
+                <h3 style="font-size: 56px; font-weight: bold; font-family: Arial, sans-serif;">
+    <span style="color: #9FE2BF;">Ski</span>fun Training Center
+</h3>                    <p style = "font-size:20px; font-weight: bold;">Experience the largest indoor <br> winter facility in Kenya.</p>
+                </div>                  
                             </div>
                         </div>
                     </div>
@@ -66,7 +46,7 @@
                     <div class="about_info">
                         <div class="section_title mb-20px">
                             <span>About Us</span>
-                            <h3>Welcome to <br> Skifun training <br> center Ltd. <br>
+                            <h3>Welcome to <br> Skifun training <br> center Kenya. <br>
                                 </h3>
                         </div>
                         <p>Plan your next vacation with us and experience the largest indoor winter facility. We specialized in indoor volant powder skis where we use modern technology to maximize your enjoyment on the deep, untracked snow.</p>
@@ -87,15 +67,15 @@
         </div>
     </div>
     <!-- about_area_end -->
-<style>
+<!-- <style>
     .single_offers .about_thumb img {
     height: 300px; /* Adjust the height as needed */
     width: 100%; /* Maintain aspect ratio */
     object-fit: cover; /* Ensure the image covers the entire container */
 }
 </style>
-    <!-- offers_area_start -->
-    <div class="offers_area">
+     offers_area_start -->
+    <!-- <div class="offers_area">
         <div class="container">
             <div class="row">
                 <div class="col-xl-12">
@@ -153,7 +133,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>  -->
     <!-- offers_area_end -->
 
     <!-- video_area_start -->
@@ -190,9 +170,7 @@
                             <h3>We Serve Fresh and <br>
                                 Delicious Food</h3>
                         </div>
-                        <p>Suscipit libero pretium nullam potenti. Interdum, blandit phasellus consectetuer dolor ornare
-                            dapibus enim ut tincidunt rhoncus tellus sollicitudin pede nam maecenas, dolor sem. Neque
-                            sollicitudin enim. Dapibus lorem feugiat facilisi faucibus et. Rhoncus.</p>
+                        <p style="font-family: 'Roboto', sans-serif;">Indulge in a culinary journey like no other as we proudly serve a fusion of Kenyan, American, and French dishes. Savor the vibrant flavors of Kenya, experience the comfort of American classics, and delight in the sophistication of French cuisine—all under one roof. Our diverse menu offers something for every palate, ensuring a memorable dining experience for you and your companions. Join us and embark on a gastronomic adventure filled with exquisite tastes and delightful surprises. Bon appétit! 🥂</p>
                         <a href="#" class="line-button">Learn More</a>
                     </div>
                 </div>
@@ -200,15 +178,21 @@
         </div>
     </div>
     <!-- about_area_end -->
-
+<style>
+    .single_rooms .room_thumb img {
+    height: 400px; /* Adjust the height as needed */
+    width: 100%; /* Maintain aspect ratio */
+    object-fit: cover; /* Ensure the image covers the entire container */
+}
+</style>
     <!-- features_room_startt -->
     <div class="features_room">
         <div class="container">
             <div class="row">
                 <div class="col-xl-12">
                     <div class="section_title text-center mb-100">
-                        <span>Featured Rooms</span>
-                        <h3>Choose a Better Room</h3>
+                        <span>Featured</span>
+                        <h3>Our Services</h3>
                     </div>
                 </div>
             </div>
@@ -216,11 +200,11 @@
         <div class="rooms_here">
             <div class="single_rooms">
                 <div class="room_thumb">
-                    <img src="img/rooms/1.png" alt="">
+                    <img src="img/rooms/indoor2.jpg" alt="">
                     <div class="room_heading d-flex justify-content-between align-items-center">
                         <div class="room_heading_inner">
-                            <span>From $250/night</span>
-                            <h3>Superior Room</h3>
+                            <span> $250/night</span>
+                            <h3 style="color:#89CFF0">Superior Room</h3>
                         </div>
                         <a href="#" class="line-button">book now</a>
                     </div>
@@ -228,35 +212,35 @@
             </div>
             <div class="single_rooms">
                 <div class="room_thumb">
-                    <img src="img/rooms/2.png" alt="">
+                    <img src="img/rooms/indoor5.png" alt="">
                     <div class="room_heading d-flex justify-content-between align-items-center">
-                        <div class="room_heading_inner">
-                            <span>From $250/night</span>
-                            <h3>Deluxe Room</h3>
-                        </div>
+                    <div class="room_heading_inner">
+                            <span>Per child up to 14 years.</span>
+                            <h3 style="color:#89CFF0">Ksh 1,200</h3>
+                        </div>                        
                         <a href="#" class="line-button">book now</a>
                     </div>
                 </div>
             </div>
             <div class="single_rooms">
                 <div class="room_thumb">
-                    <img src="img/rooms/3.png" alt="">
+                    <img src="img/rooms/hero21.jpg" alt="">
                     <div class="room_heading d-flex justify-content-between align-items-center">
-                        <div class="room_heading_inner">
-                            <span>From $250/night</span>
-                            <h3>Signature Room</h3>
-                        </div>
+                    <div class="room_heading_inner">
+                            <span>For all ages group 15 years and older <br> for an hour inclusive of the skating boots. </span>
+                            <h3 style="color:#89CFF0">Ksh 1,500</h3>
+                        </div>    
                         <a href="#" class="line-button">book now</a>
                     </div>
                 </div>
             </div>
             <div class="single_rooms">
                 <div class="room_thumb">
-                    <img src="img/rooms/4.png" alt="">
+                    <img src="img/rooms/02.webp" alt="">
                     <div class="room_heading d-flex justify-content-between align-items-center">
                         <div class="room_heading_inner">
-                            <span>From $250/night</span>
-                            <h3>Couple Room</h3>
+                        <span>Monthly rates per child/adult </span>
+                            <h3 style="color:#89CFF0">Ksh 23,000</h3>
                         </div>
                         <a href="#" class="line-button">book now</a>
                     </div>
